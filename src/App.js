acdf2507c9e6d81a67fd1294b61819f12a0e948d
Jose Luis Matallana Castellanos <joseluismatallanacastellanos@googlemail.com>
2023-09-17 20:15:35 -0500

    PreEntrega 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/NavBar';
+import NavBar from './components/NavBar';
+import FooTer from './components/FooTer';
+import CarD from './components/CarD';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <NavBar/>
       </header>
+      <section>
+        <article>
+          <div className='row row-cols-1 row-cols-md-3 mb-3 text-center'>
+            <CarD id='card1' cardTitle='Producto 1' cardPrice='100.000' cardfeatures1='Ancho: 20cm'
+              cardfeatures2='Alto: 20cm' cardfeatures3='Color: Rojo' cardfeatures4='Stock: 100'/>
+            <CarD id='card2' cardTitle='Producto 2' cardPrice='100.000' cardfeatures1='Ancho: 20cm'
+              cardfeatures2='Alto: 20cm' cardfeatures3='Color: Rojo' cardfeatures4='Stock: 100'/>
+            <CarD id='card3' cardTitle='Producto 3' cardPrice='100.000' cardfeatures1='Ancho: 20cm'
+              cardfeatures2='Alto: 20cm' cardfeatures3='Color: Rojo' cardfeatures4='Stock: 100'/>
+          </div>
+        </article>
+      </section>
+      <footer>
+        <FooTer/>  
+      </footer>
     </div>
   );
 }
